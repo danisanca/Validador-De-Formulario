2934cf19c5a92efed91d8e70cafec6738ae2e36a
Daniel Mendonça <dani_sanca@hotmail.com>
2022-01-01 12:10:06 -0300

    Adicionando validador ao git

---
+++ script.js
@@ -0,0 +1,77 @@
+let form = document.querySelector('.b7validator');
+let b7Validator = {
+    handlesSubimit:(event)=>{
+        event.preventDefault();
+        let send = true;
+        let inputs=form.querySelectorAll('input');
+
+        b7Validator.clearErrors();
+
+        for(let i = 0;i<inputs.length;i++){
+            let input = inputs[i];
+            let check = b7Validator.checkInput(input);
+
+            if(check !== true){
+                send=false;
+                b7Validator.showError(input,check);
+            }
+        }
+        if(send){
+            form.submit();
+        }
+    },
+    checkInput:(input)=>{
+        let rules = input.getAttribute('data-rules');
+
+        if(rules !== null){
+            rules = rules.split('|');
+           
+            for(let k in rules){
+                let rDetails = rules[k].split('=');
+
+                switch(rDetails[0]){
+                    case 'required':
+                        if(input.value == ''){
+                            return 'Campo Vazio.'
+                        }                    
+                    break;
+                    
+                    case 'min':
+                        if(input.value.length < rDetails[1]){
+                            return `O Campo precisa ter no minimo ${rDetails[1]} Caracters`
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != ""){
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())){
+                                return "Digite um email valido"
+                            }
+                        }
+                    break;
+                }
+            }
+
+        }
+        return true;
+    },
+    showError:(input,error)=>{
+        input.style.borderColor="#FF0000";
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+        input.parentElement.insertBefore(errorElement,input.ElementSibling);
+    },
+    clearErrors:()=>{
+        let inputs = form.querySelectorAll('input');
+            for(let i =0 ; i<inputs.length;i++){
+                inputs[i].style='';
+            }
+
+        let errorElements=document.querySelectorAll('.error');
+        for(let i=0; i < errorElements.length; i++){
+            errorElements[i].remove();
+        }
+    }
+}
+form.addEventListener('submit',b7Validator.handlesSubimit);
